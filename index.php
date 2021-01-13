f00c1569789b70022cb5c996cbc079c9008a1b60
GIULLIANO FELIPE PELLICIARI PASTOR <987325348@itaud.des.ihf>
2021-01-13 14:47:46 -0300

    pronto

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
 O JOGO
+
+<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.change.org%2Fp%2Fpresident-of-united-states-acabar-com-o-jogo&psig=AOvVaw214e9IjMoTUwOVn6OKWhCJ&ust=1610646381564000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIiqzcG7me4CFQAAAAAdAAAAABAD">
